2c0e20a3f948cf37b9a67fa26c46b5de1f8a7397
tatain1 <tatain1@hotmail.com>
2018-01-23 13:11:17 +0100

    3 premier dés

---
+++ game.js
@@ -0,0 +1,4 @@
+//creation de l'arene
+var des1 = new DesSex();
+var des2 = new DesColor();
+var des3 = new DesArme();
--- index.php
+++ index.php
@@ -1,24 +1,27 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
 <head>
     <meta charset="UTF-8">
     <title>Find The Ninja</title>
 </head>
 <body>
     <!-- <script src="classes/Personnage.js"></script> -->
-    <script src="ninja.js"></script>
-    <script src="game.js"></script>
+    <script type="text/javascript" src="ninja.js"></script>
+    <script type="text/javascript" src="des.js"></script>
+    <script type="text/javascript" src="game.js"></script>
 
     <p>
       Vous devez trouver le ninja correspondant à cette description :
-      <span id="D1"></span> <span id="D2"></span> & <span id="D3"></span>
+      <span id="D1"></span>, <span id="D2"></span> & <span id="D3"></span>
     </p>
 
     <script type="text/javascript">
-      console.log(arene);
-      document.getElementById('D1').innerHTML = d1.tirage;
-      document.getElementById('D2').innerHTML = d2.tirage;
-      document.getElementById('D3').innerHTML = d3.tirage;
+      console.log(des1);
+      console.log(des2);
+      console.log(des3);
+      document.getElementById('D1').innerHTML = des1.result;
+      document.getElementById('D2').innerHTML = des2.result;
+      document.getElementById('D3').innerHTML = des3.result;
     </script>
 
 </body>
