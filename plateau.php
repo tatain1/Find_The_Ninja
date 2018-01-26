f275505ab3f30b9f4830443f2097b269952bacd3
tatain1 <tatain1@hotmail.com>
2018-01-26 16:22:10 +0100

    Nouveau Plateau (suite)

--- plateau.php
+++ plateau.php
@@ -3,6 +3,8 @@
 <head>
     <meta charset="UTF-8">
     <title>Find The Ninja</title>
+    <link rel="stylesheet" href="normalize.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
     <link rel="stylesheet" href="style.css">
 </head>
 
@@ -13,32 +15,52 @@ shuffle($my_arrays);
 
 <body>
   <!-- ZONE DE TEST  -->
+  <!-- <div class="rules">
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+  </div> -->
   <div class="plateauTest">
     <div class="ligneTest">
+      <div class="vide15"></div>
       <div class="tuileTest1"><img src="images/ninjatest.png" class="ninjatest"></div>
       <div class="tuileTest2"><img src="images/ninjatest.png" class="ninjatest"></div>
       <div class="tuileTest1"><img src="images/ninjatest.png" class="ninjatest"></div>
-      <div class="tuileTest2"><img src="images/ninjatest.png" class="ninjatest"></div>
+      <div class="tuileTest2"><p style="width:100%;text-align:center;margin-bottom:0px;">ICI PORTE FIXE</p></div>
       <div class="tuileTest1"><img src="images/ninjatest.png" class="ninjatest"></div>
       <div class="tuileTest2"><img src="images/ninjatest.png" class="ninjatest"></div>
       <div class="tuileTest1"><img src="images/ninjatest.png" class="ninjatest"></div>
-      <div class="tuileTest2"><img src="images/ninjatest.png" class="ninjatest"></div>
-      <div class="tuileTest1"><img src="images/ninjatest.png" class="ninjatest"></div>
-      <!-- <div class="tuileTest2"><img src="images/ninjatest.png" class="ninjatest"></div> -->
     </div>
     <div class="ligneTest2">
+      <div class="vide10"></div>
       <div class="tuileTest2"><img src="images/ninjatest.png" class="ninjatest"></div>
+      <div class="vide60"><p style="width:100%;text-align:center;margin-bottom:0px;">Vous recherchez :</p></div>
       <div class="tuileTest1"><img src="images/ninjatest.png" class="ninjatest"></div>
     </div>
     <div class="ligneTest">
-
+      <div class="vide10"></div>
+      <div class="tuileTest2"><img src="images/ninjatest.png" class="ninjatest"></div>
+      <div class="vide15"><p style="width:100%;text-align:center;margin-bottom:0px;"><?php echo $my_arrays[0] ?></p></div>
+      <div class="vide15"><p style="width:100%;text-align:center;margin-bottom:0px;"><?php echo $my_arrays[1] ?></p></div>
+      <div class="vide15"><p style="width:100%;text-align:center;margin-bottom:0px;"><?php echo $my_arrays[2] ?></p></div>
+      <div class="vide15"><p style="width:100%;text-align:center;margin-bottom:0px;"><?php echo $my_arrays[3] ?></p></div>
+      <div class="tuileTest1"><img src="images/ninjatest.png" class="ninjatest"></div>
     </div>
     <div class="ligneTest2">
-
+      <div class="vide10"></div>
+      <div class="tuileTest2"><img src="images/ninjatest.png" class="ninjatest"></div>
+      <div class="vide60"></div>
+      <div class="tuileTest1"><img src="images/ninjatest.png" class="ninjatest"></div>
     </div>
     <div class="ligneTest">
-
+      <div class="vide20"></div>
+      <div class="tuileTest1"><img src="images/ninjatest.png" class="ninjatest"></div>
+      <div class="tuileTest2"><img src="images/ninjatest.png" class="ninjatest"></div>
+      <div class="tuileTest1"><img src="images/ninjatest.png" class="ninjatest"></div>
+      <div class="tuileTest2"><img src="images/ninjatest.png" class="ninjatest"></div>
+      <div class="tuileTest1"><img src="images/ninjatest.png" class="ninjatest"></div>
+      <div class="tuileTest2"><img src="images/ninjatest.png" class="ninjatest"></div>
     </div>
+
+
   </div>
 
 </body>
